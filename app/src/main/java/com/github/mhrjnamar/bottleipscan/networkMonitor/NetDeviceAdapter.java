fd6f87b24cb577dcc608f229ce2c17bd8902126c
Amar Maharjan <amar@bottle.com.np>
2019-07-17 16:23:00 +0545

    Initial commit

---
+++ app/src/main/java/com/github/mhrjnamar/bottleipscan/networkMonitor/NetDeviceAdapter.java
@@ -0,0 +1,97 @@
+package com.github.mhrjnamar.bottleipscan.networkMonitor;
+
+import android.content.Context;
+
+import android.view.ViewGroup;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.mhrjnamar.bottleipscan.R;
+import com.github.mhrjnamar.bottleipscan.netTools.model.Device;
+
+import java.net.InetAddress;
+import java.util.List;
+
+/**
+ * Created by dan on 10/22/14.
+ */
+public class NetDeviceAdapter extends RecyclerView.Adapter<NetDeviceAdapter.ViewHolder> {
+
+
+    private List<Device> addresses;
+    private int rowLayout;
+
+
+
+    private Context mContext;
+
+    public NetDeviceAdapter(List<Device> addresses, int rowLayout, Context mContext) {
+        this.addresses = addresses;
+        this.rowLayout = rowLayout;
+        this.mContext = mContext;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(rowLayout, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        Device address = addresses.get(i);
+        viewHolder.deviceName.setText(address.getDeviceName());
+        viewHolder.deviceIp.setText(address.getIpAddress());
+        viewHolder.macAdd.setText(address.getMacAddress());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return addresses == null ? 0 : addresses.size();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public List<Device> getAddresses() {
+        return addresses;
+    }
+
+    /**
+     *
+     * @param addresses
+     */
+    public void setAddresses(List<Device> addresses) {
+        this.addresses = addresses;
+    }
+
+    public Context getmContext() {
+        return mContext;
+    }
+
+    public void setmContext(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView deviceName;
+        public TextView deviceIp;
+        public TextView macAdd;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            deviceName = (TextView) itemView.findViewById(R.id.deviceName);
+            deviceIp = (TextView) itemView.findViewById(R.id.deviceIp);
+            macAdd = (TextView)itemView.findViewById(R.id.macAdd);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/github/mhrjnamar/bottleipscan/networkMonitor/Scan.java
@@ -0,0 +1,207 @@
+package com.github.mhrjnamar.bottleipscan.networkMonitor;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+
+import android.os.Bundle;
+
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialog;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import com.github.mhrjnamar.bottleipscan.R;
+import com.github.mhrjnamar.bottleipscan.netTools.Pinger;
+import com.github.mhrjnamar.bottleipscan.netTools.model.Device;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Scan extends AppCompatActivity {
+
+    private NetDeviceAdapter adapter = new NetDeviceAdapter(new ArrayList<Device>(15), R.layout.device_fragment, this);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.mainView);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        display.getMetrics(outMetrics);
+
+        float density = getResources().getDisplayMetrics().density;
+        float dpHeight = outMetrics.heightPixels / density;
+        float dpWidth = outMetrics.widthPixels / density;
+
+        int numrows = (int) Math.floor(dpWidth / 300);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
+
+        StaggeredGridLayoutManager manager = new StaggeredGridLayoutManager(numrows, StaggeredGridLayoutManager.VERTICAL);
+        manager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setHasFixedSize(false);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        rescan();
+    }
+
+    private void rescan() {
+
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        if (mWifi.isConnected() && mWifi.isAvailable()) {
+            AppCompatDialog dialog = new AppCompatDialog(this);
+            dialog.setTitle(R.string.scanning);
+            dialog.setCancelable(false);
+            dialog.show();
+            AsyncScan scan = new AsyncScan(dialog, getString(R.string.scanning_your_network));
+            scan.execute(adapter);
+
+        } else {
+            Toast.makeText(this, getString(R.string.not_connected_error), Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.scan, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_refresh:
+                rescan();
+            default:
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        // Destroy
+        super.onDestroy();
+    }
+
+    public static String intToIp(int i) {
+
+        return ((i >> 24) & 0xFF) + "." +
+                ((i >> 16) & 0xFF) + "." +
+                ((i >> 8) & 0xFF) + "." +
+                (i & 0xFF);
+    }
+
+    private static class AsyncScan extends AsyncTask<NetDeviceAdapter, Void, List<Device>> {
+
+        private NetDeviceAdapter adapter;
+        private AppCompatDialog mDialog;
+
+
+        public AsyncScan(AppCompatDialog dialog, String string) {
+            super();
+            this.mDialog = dialog;
+        }
+
+        @Override
+        protected List<Device> doInBackground(NetDeviceAdapter... voids) {
+
+
+            String ipString = getLocalIpv4Address();
+
+
+            if (ipString == null) {
+                return new ArrayList<Device>(1);
+            }
+            int lastdot = ipString.lastIndexOf(".");
+            ipString = ipString.substring(0, lastdot);
+
+            List<Device> addresses = Pinger.getDevicesOnNetwork(ipString);
+            adapter = voids[0];
+            return addresses;
+        }
+
+        @Override
+        protected void onPostExecute(List<Device> inetAddresses) {
+            super.onPostExecute(inetAddresses);
+            adapter.setAddresses(inetAddresses);
+            adapter.notifyDataSetChanged();
+            mDialog.cancel();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+
+        }
+
+
+        public static String getLocalIpv4Address() {
+            try {
+                String ipv4;
+                List<NetworkInterface> nilist = Collections.list(NetworkInterface.getNetworkInterfaces());
+                if (nilist.size() > 0) {
+                    for (NetworkInterface ni : nilist) {
+                        List<InetAddress> ialist = Collections.list(ni.getInetAddresses());
+                        if (ialist.size() > 0) {
+                            for (InetAddress address : ialist) {
+                                ipv4 = address.getHostAddress();
+                                boolean isIPv4 = ipv4.indexOf(':')<0;
+                                if (!address.isLoopbackAddress() && isIPv4){
+                                    return ipv4;
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+            } catch (SocketException ignored) {
+
+            }
+            return "";
+        }
+
+
+    }
+}
